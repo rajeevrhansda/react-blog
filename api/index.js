0c6f2d60669a72afc235f1e7226b5d2a4f2605fd
Rajeev <rajeevrhansda@gmail.com>
2022-07-16 01:31:52 +0530

    create user mongodb

--- api/index.js
+++ api/index.js
@@ -2,21 +2,22 @@ const express = require("express")
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json());
 
 
-// MONGO_URL = mongodb+srv://blog:admin123@cluster0.htnet.mongodb.net/?retryWrites=true&w=majority
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-        useFindAndModify: true
     })
     .then(console.log("Connected to MongoDB"))
     .catch((error) => console.log("ERROR",error));
 
+    app.use("/api/auth", authRoute);
 app.listen("5000", () => {
     console.log("Backend is running");
 })
