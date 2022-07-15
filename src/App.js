6d519879d371e9a52c5f1954b22cfa057467a01a
Rajeev <rajeevrhansda@gmail.com>
2022-07-15 08:33:25 +0530

    https://fontawesome.com/

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
+      <Topbar/>
       
     </div>
   );
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './topbar.css'
+export default function Topbar() {
+  return (
+    <div className='top'>
+      <div className="topLeft"> <i class="fa-brands fa-facebook-square"></i>top left</div>
+      <div className="topCenter">top center</div>
+      <div className="topRight">top right</div>
+    </div>
+  )
+}
