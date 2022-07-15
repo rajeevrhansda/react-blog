91daeb27055665dd01041d8822a755e1977a1e03
Rajeev <rajeevrhansda@gmail.com>
2022-07-16 00:35:52 +0530

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,22 @@
 const express = require("express")
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.use("/", (req, res)=>{
-    console.log("Hey! this is main url");
-})
+dotenv.config();
 
-app.listen("5000", ()=>{
+
+// MONGO_URL = mongodb+srv://blog:admin123@cluster0.htnet.mongodb.net/?retryWrites=true&w=majority
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((error) => console.log("ERROR",error));
+
+app.listen("5000", () => {
     console.log("Backend is running");
 })
