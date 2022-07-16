df613453a75c4c356889999c7be5aa9f6e210cb6
Rajeev <rajeevrhansda@gmail.com>
2022-07-16 08:16:28 +0530

    user posts no image upload

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts")
+const categoryRoute = require("./routes/categories")
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +21,11 @@ mongoose
     .catch((error) => console.log("ERROR",error));
 
     app.use("/api/auth", authRoute);
+    app.use("/api/users", userRoute);
+    app.use("/api/posts", postRoute);
+    app.use("/api/categories", categoryRoute);
+
+
 app.listen("5000", () => {
     console.log("Backend is running");
 })
