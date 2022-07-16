c2f9490cee1c1fe7c0abfae1740417e178b52137
Rajeev <rajeevrhansda@gmail.com>
2022-07-16 09:15:53 +0530

    multer image upload

--- api/index.js
+++ api/index.js
@@ -6,9 +6,24 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
 const postRoute = require("./routes/posts")
 const categoryRoute = require("./routes/categories")
+const multer = require("multer");
+
 
 dotenv.config();
 app.use(express.json());
+
+const storage = multer.diskStorage({
+    destination: (req,file, cb)=>{
+        cb(null, "images");
+    },
+    filename: (req,file,cb)=>{
+        cb(null, "hello.png");
+    },
+});
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded")
+})
 
 
 mongoose
