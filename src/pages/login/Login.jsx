5e2cbfa4b369a35f72f372f76224fe76b031938c
rajeevrhansda <rajeevrhansda@gmail.com>
2022-08-08 06:34:03 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
         e.preventDefault();
         dispatch({ type: "LOGIN_START" })
         try {
-            const res = await axios.post("/auth/login", {
+            const res = await axios.post("https://react-blog01.herokuapp.com/api/auth/login", {
                 username: userRef.current.value,
                 password: passwordRef.current.value,
             });
